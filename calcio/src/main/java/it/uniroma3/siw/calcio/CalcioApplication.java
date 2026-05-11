cd3f7cb9d1e94071537123a1ba8b778b84d1d41e
Riccardo <111280428+riccardo1512@users.noreply.github.com>
2026-05-11 18:43:03 +0200

    first commit

---
+++ calcio/src/main/java/it/uniroma3/siw/calcio/CalcioApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.calcio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalcioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalcioApplication.class, args);
+	}
+
+}
---
+++ calcio/src/test/java/it/uniroma3/siw/calcio/CalcioApplicationTests.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.calcio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalcioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
